89321e5169cdb0b7ee46b649f72c1a0d611c8ed4
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-10-23 13:24:14 +0100

    Updated index.php

--- index.php
+++ index.php
@@ -10,8 +10,40 @@
 <body>
     <main>
         <header>
-            <h1>Hello World</h1>
+            <h1>PDO</h1>
         </header>
+        <section>
+            <h2>Read Data</h2>
+            <form action="<?php echo $_SERVER['PHP_SELF']?>" method="GET">
+               <label for="city">
+                City Name
+               </label>
+               <input type="text" id="city" name="city" required />
+               <button>Submit</button>
+            </form>
+        </section>
+        <section>
+            <h2>Insert Data</h2>
+            <form action="<?php echo $_SERVER['PHP_SELF']?>" method="POST">
+               <label for="newcity">
+                City Name
+               </label>
+               <input type="text" id="newcity" name="newcity" required />
+               <label for="countrycode">
+                 Country Code 
+               </label>
+               <input type="text" id="countrycode" name="countrycode" maxlenght="3" required />
+               <label for="district">
+                  District
+               </label>
+               <input type="text" id="district" name="district" required />
+               <label for="population">
+                  Population
+               </label>
+               <input type="text" id="population" name="population" required />
+               <button>Submit</button>
+            </form>
+        </section>
     </main>
 </body>
 </html>
